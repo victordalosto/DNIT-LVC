56e19dbc472bb092aeb449ab98822a2929979ab1
VictorDalosto <victordalosto@gmail.com>
2023-07-27 11:20:10 -0300

    remove unused files

--- Main.py
+++ Main.py
@@ -1,7 +1,28 @@
 import src.dnit.LVCcheck as LVC
 
-hd = "F:\\4302"
-trechos = ["full"]
+hd = "\\\\10.100.10.219\\Videos$\\Recebidos\\2023\\Lote1\\4101"
+trechos = [200,
+212,
+223,
+188,
+184,
+113,
+114,
+97,
+98,
+100,
+101,
+125,
+144,
+66,
+74,
+75,
+87,
+50,
+268,
+262,
+161]
+
 LVC.check(hd, trechos)
 
 
